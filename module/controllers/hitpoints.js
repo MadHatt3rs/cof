e56cb08161814e73c90f5ebcf25c13f30c5a27e5
Thomas <guegnont@gmail.com>
2021-10-25 20:16:19 +0200

    Correction du RD et de l'application des PV

--- module/controllers/hitpoints.js
+++ module/controllers/hitpoints.js
@@ -10,8 +10,12 @@ export class Hitpoints {
                 let data = duplicate(target.actor.data);
                 let hp = data.data.attributes.hp;
                 // Application de la RD si c'est cochée
-                const finalAmount = amount + (dr ? data.data.attributes.dr.value : 0);
+                let finalAmount = amount + (dr ? data.data.attributes.dr.value : 0);
+                finalAmount = (finalAmount > 0 && dr ? 0 : finalAmount);
+                
                 hp.value += finalAmount;
+                hp.value = (hp.value > hp.max ? hp.max : hp.value);
+                hp.value = (hp.value < 0 ? 0 : hp.value);
 
                 target.actor.update(data);
             }
@@ -27,7 +31,7 @@ export class Hitpoints {
             case "full"   : Hitpoints.applyToTargets(-dmg, dr); break;
             case "half"   : Hitpoints.applyToTargets(-Math.ceil(dmg/2), dr); break;
             case "double" : Hitpoints.applyToTargets(-dmg*2, dr); break;
-            case "heal"   : Hitpoints.applyToTargets(dmg, dr); break;
+            case "heal"   : Hitpoints.applyToTargets(dmg, false); break;
         }
     }
 }
